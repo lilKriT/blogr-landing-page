fc1cc5936f9117798ef24fe051199518860cd46f
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-18 20:58:47 +0200

    Buttons work.

--- script.js
+++ script.js
@@ -3,5 +3,25 @@ console.log("Script loaded");
 
 // Finding elements
 const openMobileMenuButton = document.querySelector("#openMobileMenuButton");
+const closeMobileMenuButton = document.querySelector("#closeMobileMenuButton");
 
 // Functions
+const openMobileMenu = () => {
+  console.log("Opening mobile menu");
+
+  openMobileMenuButton.classList.remove("block");
+  openMobileMenuButton.classList.add("hidden");
+  closeMobileMenuButton.classList.remove("hidden");
+  closeMobileMenuButton.classList.add("block");
+};
+openMobileMenuButton.addEventListener("click", openMobileMenu);
+
+const closeMobileMenu = () => {
+  console.log("Closing mobile menu");
+
+  closeMobileMenuButton.classList.remove("block");
+  closeMobileMenuButton.classList.add("hidden");
+  openMobileMenuButton.classList.remove("hidden");
+  openMobileMenuButton.classList.add("block");
+};
+closeMobileMenuButton.addEventListener("click", closeMobileMenu);
